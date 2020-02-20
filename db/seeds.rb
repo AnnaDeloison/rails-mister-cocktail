5c3af0ea04f4e8f89e0ae12ca1b5a927b68533c9
Anna Deloison <anna.deloison@gmail.com>
2020-02-20 18:59:53 +0100

    exo done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+# Ingredient.create(name: "lemon")
+# Ingredient.create(name: "ice")
+# Ingredient.create(name: "mint leaves")
+
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+ing_serialized = open(url).read
+array = JSON.parse(ing_serialized)
+
+ingredients_args = []
+array.each do |ing|
+  ing[1].each do |e|
+    ingredients_args << e['strIngredient1']
+  end
+end
+
+ingredients_args.each do |el|
+Ingredient.create(name: el)
+end
